e10633119f5d8f34644ecf64153d76e10a561f93
rodders1991 <georgeormrod@gmail.com>
2016-03-08 21:12:53 +0000

    First Commit

---
+++ ShoppingBasket/ItemOrder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasket
+{
+    class ItemOrder
+    {
+       
+        public string name
+        {
+            get;
+            set;
+        }
+        
+        public double latestPrice
+        {
+            get;
+            set;
+        }
+
+        public int quantity
+        {
+            get;
+            set;
+        }
+
+
+        public ItemOrder(string name, double latestPrice, int quantity)
+        {
+            this.name = name;
+            this.latestPrice = latestPrice;
+            this.quantity = quantity;
+        }
+
+        public ItemOrder(string name, double latestPrice)
+        {
+            this.name = name;
+            this.latestPrice = latestPrice;
+            quantity = 1;
+        }
+
+       
+    }
+}
